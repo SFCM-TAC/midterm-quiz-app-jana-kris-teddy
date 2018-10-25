8c67ea9c83dd9bd9b250de2502bd6b621ca0045b
kgrant <kristopher.grant@sfcm.edu>
2018-10-24 17:44:38 -0700

    Files should be sent to master, JS file, HTML with frame, CSS with basic decorations

---
+++ js/script.js
@@ -0,0 +1,10 @@
+//Michael Jackson questions
+// How many brothers did Michael Jackson have? 6
+// What city was Michael Jackson born in? Gary, IN
+// How many number 1 songs did Michael Jackson have? 10 songs
+// Who is Michael Jackson's youngest sibling? Janet Jackson
+// _____ is the name of Michael Jackson's oldest son?  Prince
+//Michael Jackson won this many Emmy Awards: 0 (13 GRAMMY awards)
+//Michael launched what product in 1986 based off of animals he owned? Michaels Pets
+//The homestead on which Michael Jackson lived is called: Neverland Ranch
+//At his 1988 London concert, what song did Michael Jackson include in his setlist at Princess Diana's request? Dirty Di
