d86c213065a2235184fdd5dfd073cd6337124ca5
Teddy Raven <teddyraven@Teddys-MacBook-Pro.local>
2018-10-29 16:17:40 -0700

    Completed quiz- JS interacts with HTML, goes onto new questions upon button click and tallies up correct points giving an end result. styling could be imporved though!

--- js/script.js
+++ js/script.js
@@ -5,8 +5,8 @@ function Question(text, choices, answer) {
     this.answer = answer;
 }
 
-Question.prototype.correctAnswer = function(choice) {
-    return choice === this.answer;
+Question.prototype.isCorrectAnswer = function(choice) {
+    return this.answer === choice;
 }
 
 function Quiz(questions) {
@@ -19,21 +19,23 @@ Quiz.prototype.getQuestionIndex = function() {
   return this.questions[this.questionIndex];
 }
 
-Quiz.prototype.isEnded = function() {
-    return this.questions.length === this.questionIndex;
-}
-
 Quiz.prototype.guess = function(answer) {
-    this.questionIndex++;
-
-    if(this.getQuestionIndex().correctAnswer(answer)) {
+    if(this.getQuestionIndex().isCorrectAnswer(answer)) {
         this.score++;
     }
+
+this.questionIndex++;
 }
+
+Quiz.prototype.isEnded = function() {
+    return this.questionIndex === this.questions.length;
+}
+
+
 
 function populate() {
   if(quiz.isEnded()) {
-    //showScores();
+    showScores();
   }
   else {
     //show question
@@ -44,9 +46,40 @@ function populate() {
     for(var i=0; i< choices.length; i++) {
         var element = document.getElementById("choice" + i);
         element.innerHTML = choices[i];
-    }
-      }
+        guess("btn" + i, choices[i]);
+          }
+
+            showProgress();
+
         }
+      };
+
+function guess(id, guess) {
+  var button = document.getElementById(id);
+  button.onclick = function() {
+    quiz.guess(guess);
+    populate();
+  }
+};
+
+function showProgress() {
+    var currentQuestionNumber = quiz.questionIndex + 1;
+    var element = document.getElementById("progress");
+    element.innerHTML = "Question " + currentQuestionNumber + " of " + quiz.questions.length;
+
+};
+
+
+function showScores() {
+    var quizOverHTML = "<h1>Result</h1>";
+        quizOverHTML += "<h2 id='score'> Your scores: " + quiz.score + "</h2>";
+    var element = document.getElementById("quiz");
+    element.innerHTML = quizOverHTML;
+
+};
+
+
+
 
 var questions = [
     new Question("How many brothers did Michael Jackson have?", ["2", "4", "5", "6"], "6"),
